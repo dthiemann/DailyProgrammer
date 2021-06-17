f9b5c33a719d42993ec5bcd80d17080ee4c849f4
Dylan Thiemann <dylanthiemann@gmail.com>
2021-06-16 22:59:25 -0500

    Copied solution for find smallest missing number

--- InterviewPractice/strings.py
+++ InterviewPractice/strings.py
@@ -142,15 +142,59 @@ def findWordHelperDef(board, i, j, word):
     )
 
 
-print(
-    findWord(
-        [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "ABCCED"
-    )
-)
-print(
-    findWord([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "SEE")
-)
-print(
-    findWord([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "ABCB")
-)
+# print(
+#     findWord(
+#         [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "ABCCED"
+#     )
+# )
+# print(
+#     findWord([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "SEE")
+# )
+# print(
+#     findWord([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "ABCB")
+# )
+
+"""
+Given an unsorted integer array, find the smallest missing positive integer...
+Your algorithm should run in O(n) time and uses constant extra space.
+
+Input: nums = [1,2,0]
+Output: 3
+
+Input: nums = [3,4,-1,1]
+Output: 2
+
+Input: nums = [7,8,9,11,12]
+Output: 10
+"""
+
+
+def findSmallestMissingPositiveInteger(nums):
+
+    # :type nums: List[int]
+    # :rtype: int
+    #  Basic idea:
+    # 1. for any array whose length is l, the first missing positive must be in range [1,...,l+1],
+    #     so we only have to care about those elements in this range and remove the rest.
+    # 2. we can use the array index as the hash to restore the frequency of each number within
+    #      the range [1,...,l+1]
+
+    nums.append(0)
+    n = len(nums)
+    for i in range(len(nums)):  # delete those useless elements
+        if nums[i] < 0 or nums[i] >= n:
+            nums[i] = 0
+    for i in range(
+        len(nums)
+    ):  # use the index as the hash to record the frequency of each number
+        nums[nums[i] % n] += n
+    for i in range(1, len(nums)):
+        if nums[i] / n == 0:
+            return i
+    return n
+
+
+print(findSmallestMissingPositiveInteger([1, 2, 0]))
+print(findSmallestMissingPositiveInteger([3, 4, -1, 1]))
+print(findSmallestMissingPositiveInteger([7, 8, 9, 11, 12]))
 
