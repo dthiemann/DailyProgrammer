cbc2387a77b28a0bfb22ea4edee2adc637d626e7
dthiemann <dylanthiemann@gmail.com>
2021-06-22 21:41:04 -0500

    Palindrome

--- InterviewPractice/Java/StringsPractice.java
+++ InterviewPractice/Java/StringsPractice.java
@@ -22,7 +22,43 @@ public class StringsPractice {
         return true;
     }
 
-    public static void main(String[] args) {
+    /*
+     * 
+     * 1.3 Given two strings, write a method to decide if one is a permutation of
+     * the other
+     * 
+     * ABCDE -- DABCE
+     */
 
+    public boolean isPalindrome(String string1, String string2) {
+        if (string1.length() != string2.length()) {
+            return false;
+        }
+
+        // Same assumption as above (ASCII string)
+        int[] buffer = new int[256];
+
+        // Populate buffer
+        for (int i = 0; i < string1.length(); i++) {
+            int charValue = string1.charAt(i);
+            buffer[charValue]++;
+        }
+
+        // Subtract other string from buffer ---
+        // If we hit < 0 return false
+        for (int i = 0; i < string2.length(); i++) {
+            int charValue = string2.charAt(i);
+            if (--buffer[charValue] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        StringsPractice sp = new StringsPractice();
+        // System.out.println(sp.isPalindrome("ABCDEF", "ABCDFE"));
+        // System.out.println(sp.isPalindrome("ABCDEF", "ABCDXE"));
     }
 }
